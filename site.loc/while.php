d867dea5dcc204eace63efa804240a246639165b
tartakovskyi <vvt2001@ukr.net>
2019-10-06 23:11:07 +0300

    array

--- site.loc/while.php
+++ site.loc/while.php
@@ -1,13 +1,32 @@
 <?php 
+$users = [
+  '2' => ['name' => 'Иван', 'email' => 'test@test.ru', 'lang' => 'ru'],
+  '5' => ['name' => 'John', 'email' => 'test@test.com', 'lang' => 'en'],
+  '7' => ['name' => 'Павло', 'email' => 'test@test.ua', 'lang' => 'uk'],
+  '8' => ['name' => 'James', 'email' => 'test2@test.com', 'lang' => 'en'],
+  '9' => ['name' => 'Петро', 'email' => 'test3@test.ua', 'lang' => 'uk'],
+  '10' => ['name' => 'Павло', 'email' => 'test4@test.ua', 'lang' => 'uk'],
+  '11' => ['name' => 'Павло', 'email' => 'test5@test.ua', 'lang' => 'uk']
+];
 
-/*ini_set('max_execution_time', 2);
-   set_time_limit(2);*/
-  $begin = microtime();
-  $i = 1;
-  while ($i <= 10000) {
-  	 echo 'test';
-  	 $i++;
-  }
-  $end = microtime();
-  echo '<br>';
-  echo $end - $begin;
+/*foreach ($users as $user) {
+  $name = $user['name'];
+  $names[$name] += 1;
+}
+foreach ($names as $key => $val) {
+   echo $key.' = '.$val.'<br>';
+ } */
+
+
+/*foreach ($users as $user) {
+  $lang = $user['lang'];
+  $languages[$lang][] = $user;
+}
+extract($languages);*/
+
+$usersNew = [];
+foreach ($users as $user) {
+array_unshift($usersNew, $user);
+}
+
+
