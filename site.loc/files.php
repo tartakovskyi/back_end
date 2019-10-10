f5e74f22965ad229769884c637338f1714de584c
tartakovskyi <vvt2001@ukr.net>
2019-10-10 22:51:57 +0300

    files

--- site.loc/files.php
+++ site.loc/files.php
@@ -1,6 +1,27 @@
 <?php 
 
-var_dump($_SERVER);
+define("PATH", "/var/www/site.loc/files/");
 
+$passString = $_POST['name'].' '.$_POST['pass'];
+
+$passwords = fopen(PATH.'passwords.txt', "r");
+
+$access = false;
+
+var_dump($passString);
+
+while (!feof($passwords)) {
+	$string = fgets($passwords);
+
+	if ($passString === preg_replace('/\n/', '', $string)) {
+		echo "Доступ разрешен";
+		$access = true;
+		break;
+	}
+}
+
+if (!$access) echo "Доступ запрещен";
 
 ?>
+
+
