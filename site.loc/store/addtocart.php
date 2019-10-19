a894786f1663938e3802f8d72030d0c590b66738
tartakovskyi <vvt2001@ukr.net>
2019-10-20 00:16:19 +0300

    store

---
+++ site.loc/store/addtocart.php
@@ -0,0 +1,8 @@
+<?php 
+
+echo json_encode($_REQUEST);
+/*var_dump($_POST);
+file_put_contents('log.txt', json_decode($_POST));*/
+
+ ?>
+
--- site.loc/store/index.php
+++ site.loc/store/index.php
@@ -14,6 +14,9 @@ $products = json_decode(file_get_contents("products.json"), true);
 	<meta charset="UTF-8">
 	<title>Магазин</title>
 	<style>
+		* {
+			font-family: sans-serif;
+		}
 		.container {
 			margin: 0 auto;
 			max-width: 1200px;
@@ -23,6 +26,10 @@ $products = json_decode(file_get_contents("products.json"), true);
 			display: -o-flex;
 			display: flex;
 			justify-content: space-around;
+		}
+		h1 {
+			text-align: center;
+			margin-bottom: 20px;
 		}
 		.prod {
 			display: -webkit-flex;
@@ -37,34 +44,90 @@ $products = json_decode(file_get_contents("products.json"), true);
 			flex-direction: column;
 			width: 19%;
 		}
-		.prod__title {
+		.prod__img {
+			position: relative;
+			padding-top: 70%;
+		}
+		.prod__img img {
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+			object-position: 50%;
+		}
+		.prod__name {
+			display: block;
 			font-size: 22px;
 			font-weight: 600;
-			margin: 20px 0 15px;
+			margin: 20px 0 10px;
+		}
+		button {
+			background-color: #f03;
+			color: #fff;
+			font-size: 18px;
+			padding: 15px 30px;
+			cursor: pointer;
+			border: none;
+		}
+		button:hover {
+			background-color: #ea345a;
+		}
+		.buy {
+			margin-top: 10px;
 		}
 	</style>
 </head>
 <body>
+	<h1>Інтернет-магазин &laquo;У Галі&raquo;</h1>
 	<div class="container">
-		
-	
-
-	<?php  foreach ($products as $prod): ?>
-
-		<div class="prod">
-			<img src="<?php echo IMG_PATH.$prod['img']  ?>" alt="">
-			<span class="prod__title"><?php echo $prod['name']  ?></span>
-			<div class="prod__charact">
-				<b>Артикул: </b><span><?php echo $prod['id'] ?></span>
+		<?php  foreach ($products as $prod): ?>
+			<div class="prod" id="<?php echo $prod['id']  ?>">
+				<div class="prod__img">
+					<img src="<?php echo IMG_PATH.$prod['img']  ?>" alt="">
+				</div>
+				<span class="prod__name" data-role="name"><?php echo $prod['name']  ?></span>
+				<div class="prod__charact">
+					<b>Артикул: </b><span ><?php echo $prod['id'] ?></span>
+				</div>
+				<div class="prod__charact">
+					<b>Ціна, грн./кг: </b><span data-role="price"><?php echo $prod['price'] ?></span>
+				</div>
+				<button class="buy" onclick="addToCart(this)">До кошику</button>
 			</div>
-			<div class="prod__charact">
-				<b>Цена, грн./кг: </b><span><?php echo $prod['price'] ?></span>
-			</div>
-		</div>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
 	</div>
 </body>
 </html>
+
+<script>
+	const addToCart = (btn) => {
+		const prod = btn.parentNode
+		const prodData = 
+		{
+			'id' : prod.id,
+			'name' : prod.querySelector('[data-role="name"]').innerText,
+			'price' : Number(prod.querySelector('[data-role="price"]').innerText),
+			'quantity' : 1
+		}
+		let response = fetch('addtocart.php', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			},
+			body: JSON.stringify(prodData)
+		})
+		.then(response => console.log(response.json()))
+
+	}
+
+	/*[].forEach.call(buttons, function(el){
+		el.addEventListener("click", function() {
+			console.log(this)
+		})
+	})*/
+	
+</script>
 
 
 
