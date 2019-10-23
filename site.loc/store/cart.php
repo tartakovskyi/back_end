f9c5f32613b2923a6b85ab973d301948c84d17d4
tartakovskyi <vvt2001@ukr.net>
2019-10-23 19:51:06 +0300

    store - cart

--- site.loc/store/cart.php
+++ site.loc/store/cart.php
@@ -1,6 +1,46 @@
 <?php 
 require_once ('config.php');
-var_dump($_SESSION);
-////
+//var_dump($_SESSION['cart']);
 
- ?>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Магазин</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Товари у кошику</h1>
+		<table class="cartTable">
+			<thead>
+				<tr>
+					<th>Артикул</th>
+					<th>Назва</th>
+					<th>Ціна, грн.</th>
+					<th>Кільість, кг</th>
+					<th>Сумма, грн.</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($_SESSION['cart'] as $prod): ?>
+					<tr>
+						<td><?php echo $prod['id'] ?></td>
+						<td><?php echo $prod['name'] ?></td>
+						<td><?php echo $prod['price'] ?></td>
+						<td><?php echo $prod['quantity'] ?></td>
+						<td><?php echo $prod['price']*$prod['quantity'] ?></td>
+					</tr>
+				<?php endforeach; ?>
+				<tr>
+					<td colspan="5" class="cartTable__total">Сумма до сплати: <?php  echo $_SESSION['total'] ?> грн.</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
+
+
--- site.loc/store/index.php
+++ site.loc/store/index.php
@@ -15,31 +15,33 @@ $products = json_decode(file_get_contents("products.json"), true);
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<h1>Інтернет-магазин &laquo;У Галі&raquo;</h1>
 	<div class="container">
-		<?php  foreach ($products as $prod): ?>
-			<div class="prod" id="<?php echo $prod['id']  ?>">
-				<div class="prod__img">
-					<img src="<?php echo IMG_PATH.$prod['img']  ?>" alt="">
+		<h1>Інтернет-магазин &laquo;У Галі&raquo;</h1>
+		<div class="wrapper">
+			<?php  foreach ($products as $prod): ?>
+				<div class="prod" id="<?php echo $prod['id']  ?>">
+					<div class="prod__img">
+						<img src="<?php echo IMG_PATH.$prod['img']  ?>" alt="">
+					</div>
+					<span class="prod__name" data-role="name"><?php echo $prod['name']  ?></span>
+					<div class="prod__charact">
+						<b>Артикул: </b><span ><?php echo $prod['id'] ?></span>
+					</div>
+					<div class="prod__charact">
+						<b>Ціна, грн./кг: </b><span data-role="price"><?php echo $prod['price'] ?></span>
+					</div>
+					<div class="quantity">
+						<button class="minus" onclick="changeQuantity(this, -1)">-</button>
+						<input type="text" name="quantity" value="0">
+						<button class="plus" onclick="changeQuantity(this, 1)">+</button>					
+					</div>
+					<button class="buy" onclick="addToCart(this)">До кошику</button>
 				</div>
-				<span class="prod__name" data-role="name"><?php echo $prod['name']  ?></span>
-				<div class="prod__charact">
-					<b>Артикул: </b><span ><?php echo $prod['id'] ?></span>
-				</div>
-				<div class="prod__charact">
-					<b>Ціна, грн./кг: </b><span data-role="price"><?php echo $prod['price'] ?></span>
-				</div>
-				<div class="quantity">
-					<button class="minus" onclick="changeQuantity(this, -1)">-</button>
-					<input type="text" name="quantity" value="0">
-					<button class="plus" onclick="changeQuantity(this, 1)">+</button>					
-				</div>
-				<button class="buy" onclick="addToCart(this)">До кошику</button>
-			</div>
-		<?php endforeach; ?>
+			<?php endforeach; ?>
+		</div>
+		<a href="cart.php">Кошик</a>
+		<?php if(isset($_SESSION['total'])): ?><div class="total">Сумма замовлення: <span id="total"><?php echo $_SESSION['total']; ?></span> грн.</div><?php endif; ?>
 	</div>
-	<a href="cart.php">Кошик</a>
-	<?php if(isset($_SESSION['total'])): ?><div class="total">Сумма замовлення: <span id="total"><?php echo $_SESSION['total']; ?></span> грн.</div><?php endif; ?>
 </body>
 </html>
 
@@ -48,7 +50,7 @@ $products = json_decode(file_get_contents("products.json"), true);
 		const prod = btn.parentNode
 		const quantity =  prod.querySelector('[name="quantity"]')
 		quantity.value = Number(quantity.value) + num
- 	}
+	}
 
 
 	const addToCart = (btn) => {
@@ -70,17 +72,17 @@ $products = json_decode(file_get_contents("products.json"), true);
 		.then(function(response) {
 			response.json().then(function(data) {  
 				changeTotal(data)
-			});  
+	});  
 
-		})
+	})
 
-		const changeTotal = (sum) => {
-			const totalElem = document.getElementById('total')
-			totalElem.innerText = sum
-		}
+	const changeTotal = (sum) => {
+		const totalElem = document.getElementById('total')
+		totalElem.innerText = sum
 	}
+}
 
-	
+
 </script>
 
 
